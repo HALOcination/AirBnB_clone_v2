f83c7decb2c6a1991c843eaf3b2254c6bc2c000a
Sahar <Aguidi53@gmail.com>
2023-09-20 05:18:53 -0700

    rework

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State Module for HBNB project"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class
+    Att:
+        name: entry name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
