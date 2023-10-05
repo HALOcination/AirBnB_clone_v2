ccbcac42e504cbdac368659c43721daa50c5117f
Sahar <Aguidi53@gmail.com>
2023-10-05 11:23:25 -0700

    fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,32 +17,33 @@ def do_deploy(archive_path):
     Returns:
         true if deployment is successful, otherwise false
     """
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        file_name = os.path.basename(archive_path)
-        name = file_name.split(".")[0]
-
-        put(archive_path, "/tmp/{}".format(file_name))
-
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, name))
-
-        run("rm /tmp/{}".format(file_name))
-
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(name, name))
-
-        run("rm -rf /data/web_static/current")
-
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
-
-        return True
-
-    except Exception as e:
-        print("An error occurred:", str(e))
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
